e5f26792fe4b2e8c36a8f9dc6cc4550fee47f5ba
Kwaku28 <rekenburg777@gmail.com>
2025-01-03 23:26:55 +0000

    Update the post page

--- src/_root/pages/CreatePost.tsx
+++ src/_root/pages/CreatePost.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import PostForm from "@/components/forms/PostForm";
 
 const CreatePost = () => {
   return (
-    <div>CreatePost</div>
-  )
-}
+    <div className="flex flex-1">
+      <div className="common-container">
+        <div>
+          <img
+            src="/assets/icons/add-post.svg"
+            alt="add"
+            width={36}
+            height={36}
+          />
+          <h2 className="h3-bold md:h2-bold">Create Post</h2>
+        </div>
 
-export default CreatePost
+        <PostForm />
+      </div>
+    </div>
+  );
+};
+
+export default CreatePost;
